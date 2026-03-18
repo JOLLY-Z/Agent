f1b0129f18f26a5726c3dbc5b0e888fd554bf77b
Jolly_Agent <18719197319@163.com>
2026-03-18 10:47:40 +0800

    浏览器具体操作

---
+++ tools/browser_tools.py
@@ -0,0 +1,14 @@
+from app.code_agent.utils.mcp import create_mcp_stdio_client
+
+
+async def get_stdio_browser_tools():
+    params = {
+        "command": "python",
+        "args": [
+            "D:/conda_envs/ai-agent-env/Lib/site-packages/app/code_agent/mcp/browser_tools.py",
+        ]
+    }
+
+    client, tools = await create_mcp_stdio_client("browser_tools", params)
+
+    return tools
